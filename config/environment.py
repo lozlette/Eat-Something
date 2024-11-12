b771a191283318a7445163fcf26f4fdb418b7a07
Lauren Bell <laurengilbert0@gmail.com>
2024-11-12 21:58:52 +0000

    pipfile lock updated

--- config/environment.py
+++ config/environment.py
@@ -1,4 +1,4 @@
 import os
 
-db_uri = os.getenv('DATABASE_URL', 'postgres://localhost:5432/eat-something')
+db_uri = os.getenv('DATABASE_URL', 'postgresql://localhost:5432/eat-something')
 secret = os.getenv('SECRET', 'shh, it\'s a secret')
